78b1a8239f88c817547a8017acccd3055b1f1994
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-03-11 10:45:24 -0300

    jwt token sign and verify

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const userService = require('../services/user');
+
+const secret = process.env.JWT_SECRET;
 
 const userCreation = async (req, res) => {
   try {
@@ -6,7 +9,15 @@ const userCreation = async (req, res) => {
     if (!userService.create) {
       return res.status(409).json({ message: 'User already registered' });
     }
-    res.status(201).json({ displayName, email, password, image });
+
+    const jwtConfig = {
+      // expiresIn: '7d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ user: { displayName, email, password, image } }, secret, jwtConfig);
+
+    res.status(201).json({ token });
   } catch (e) {
     console.log(e.message);
   }
---
+++ services/jwtAuth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/Users');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    console.log('linha 15 jwtAuth', decoded);
+
+    const user = await Users.findOne({ where: { displayName: decoded.user.displayName } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'user not found' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (e) {
+    return res.status(401).json({ message: e.message });
+  }
+};
