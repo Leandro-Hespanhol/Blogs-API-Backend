932f3f1ac485847542d221d226678f0148693ffe
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-03-11 16:45:05 -0300

    req03

--- services/jwtAuth.js
+++ services/jwtAuth.js
@@ -1,13 +1,10 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { Users } = require('../models');
 
 const secret = process.env.JWT_SECRET;
 
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
-  // console.log('TOKEN LINHA 9 AUTH', !token); 
-  console.log('TOKEN LINHA 10 AUTH', await Users); 
   
   if (!token) {
     return res.status(401).json({ message: 'Token not found' });
